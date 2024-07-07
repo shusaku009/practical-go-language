3c5482d0703df97057045bb6acba257d9ebf5848
shusaku009 <shusaku009@gmail.com>
2024-07-07 16:07:35 +0900

    add: Functional Optionパターンを使ったオプション引数

--- main.go
+++ main.go
@@ -115,3 +115,30 @@ func (o *fluentOpt) order() *Udon {
 func useFluentInterface() {
 	oomorikitsune := NewUdonOp(Large).Aburaage().order()
 }
+
+// Functional Optionパターンを使ったオプション引数
+type OptFunc func(r *Udon)
+
+func NewUdon(opts ...OptFunc) *Udon {
+	r := &Udon{}
+	for _, opt := range opts {
+		opt(r)
+	}
+	return r
+}
+
+func OptMen(p Portion) OptFunc {
+	return func(r *Udon) { r.men = p }
+}
+
+func OptAburaage() OptFunc {
+	return func(r *Udon) { r.aburaage = true }
+}
+
+func OptEbiten(n uint) OptFunc {
+	return func(r *Udon) { r.ebiten = n }
+}
+
+func useFuncOption() {
+	tokuseiUdon := NewUdon(OptAburaage(), OptEbiten(3))
+}
